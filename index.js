9e819a41980c900924e79341b69e0aa0a511ac9e
Debesh <debesh391@gmail.com>
2023-12-17 21:03:14 +0530

    files added

---
+++ index.js
@@ -0,0 +1,62 @@
+const inputElement = document.querySelector('.input-field');
+const findBtnElement = document.querySelector('.find-btn');
+const ansElement = document.querySelector('.ans-field');
+
+const filePath = 'words.txt'; 
+
+let isFound = false;
+let wordList = [];
+function isPossible(word1, word2) {
+    let v1= [];
+    let v2 = [];
+    for(let i=0; i<word1.length; i++) {
+        v1.push(word1[i]);
+    }
+    for(let i=0; i<word2.length; i++) {
+        v2.push(word2[i]);
+    }
+    v1.sort();
+    v2.sort();
+
+    if(v1.toString() === v2.toString()) {
+        return true;
+    }return false;
+    
+}
+
+///////////////////////////
+fetch(filePath)
+.then(response => {
+    if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.text();
+})
+.then(content => {
+    const words = content.split('\n').map(word => word.trim());
+
+    wordList.push(...words);
+    findBtnElement.addEventListener('click', ()=>{
+        rearrange();
+    });
+    
+})
+.catch(error => console.error('Error reading the file:', error));
+////////////////////////////
+
+function rearrange() {
+    
+    for(let i=0; i<wordList.length; i++) {
+
+        if(isPossible(wordList[i],inputElement.value)) {
+            ansElement.innerHTML = `${wordList[i]}`;
+            isFound = true;
+            break;
+        }else{
+            ansElement.innerHTML = `not found`;
+            
+        }
+    }
+    
+}
+
